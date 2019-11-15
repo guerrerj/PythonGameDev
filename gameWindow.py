91ea39cfd83fe628ca947b9258b094100dca6cad
Jose <jose.guerrero@ericsson.com>
2019-11-15 09:34:38 -0500

    Added automatic detection of parent folder 4 sprit

--- gameWindow.py
+++ gameWindow.py
@@ -13,7 +13,7 @@ from pathlib import Path
 # We use these comments to tell other people what we are doing in our code 
 
 print("Let the games begin")
-print(os.getcwd())
+print('This is the directory: {0}'.format(os.getcwd()))
 
 
 # Let us tell the game library to get ready for us (this is called initialization)
@@ -30,60 +30,71 @@ pygame.display.set_caption('Fast and Deivi')
 # Defining some colors for use in the game
 black = (0,0,0)
 white = (255, 255, 255)  # these represent colors for red blue green which can can take values from 0 to 255
-
-# Used for checking if the game has stopped
-crashed = False
+red = (255, 0, 0)
 
 # Clock used to keep track of the game events
 clock = pygame.time.Clock()
 
 # Lets load an image for our game 
-spaceshipImg = pygame.image.load('c:/Users/eseujgo/Desktop/Game/racecar.png').convert()
+spaceshipImg = pygame.image.load(str(os.getcwd()) + '/racecar.png').convert()
+# Lets choose a size for the spaceship  
+shipLength = 75
 
 # This is a function that will display our spaceship at a x and y position
 def spaceShip(x,y):
     gameDisplay.blit(spaceshipImg, (x,y)) 
 
-# This is the intial positioning of the sprite(the name of an image for 2D games)    
-x = (displayWidth) * 0.45
-y = (displayHeight) * 0.5 
+# This a function for the game loop (to keep running)
+def gameLoop():
+    # This is the intial positioning of the sprite(the name of an image for 2D games)    
+    x = (displayWidth) * 0.45
+    y = (displayHeight) * 0.5 
+    
+    # Used for checking if the game has stopped
+    gameExit = False
 
-# This the distance traveled and the speed 
-xDistanceToMove = 0
-shipSpeed = 0 
+    # This the distance traveled and the speed 
+    xDistanceToMove = 0
+    shipSpeed = 0 
 
-while not crashed:
-    
-    # Here we are checking if someone pressed the close button on our game window to quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+    while not gameExit:
+        
+        # Here we are checking if someone pressed the close button on our game window to quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+                
+            # Here we are checking to see if any buttons were pressed on keyboard to move the spaceship
+            # We do this by changing the value of the xDistanceToMove and telling the ship to go there
             
-    # Here we are checking to see if any buttons were pressed on keyboard to move the spaceship
-    # We do this by changing the value of the xDistanceToMove and telling the ship to go there
-    
-    # Checking the down key
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            xDistanceToMove = -5
-        elif event.key == pygame.K_RIGHT:
-            xDistanceToMove = 5
-            
-    # Checking the left and right keys 
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            xDistanceToMove = 0
-    
-    # Here we are changing the x variable to the new x position so we can tell the spaceship to move there
-    x = x + xDistanceToMove
-    
-    # Here we are changing the background of the game and the spaceship at its current location        
-    gameDisplay.fill(white)
-    spaceShip(x,y)
-    
-    # Here we are showing the changes to our game screen        
-    pygame.display.update()
-    clock.tick(60)
+            # Checking the down key
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    xDistanceToMove = -5
+                elif event.key == pygame.K_RIGHT:
+                    xDistanceToMove = 5
+                    
+            # Checking the left and right keys 
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    xDistanceToMove = 0
+        
+        # Here we are changing the x variable to the new x position so we can tell the spaceship to move there
+        x = x + xDistanceToMove
+        
+        # Here we are changing the background of the game and the spaceship at its current location        
+        gameDisplay.fill(white)
+        spaceShip(x,y)
+        
+        # We quit the game if spaceship goes outside screen
+        if x > displayWidth - shipLength or x < 0:
+            gameExit = True 
+        # Here we are showing the changes to our game screen        
+        pygame.display.update()
+        clock.tick(60)
+
+# Lets run the game loop, we have to call the function
+gameLoop()
 
 # If we exit the loop means program ended then we close the game library
 print('Goodbye') 
